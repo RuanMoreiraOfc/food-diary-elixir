24d6c64e86604b348a91caaa67457e3d67329054
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-09-07 08:36:28 -0300

    feat(UserResolver): add `create` resolver function

--- lib/food_diary_web/resolvers/user.ex
+++ lib/food_diary_web/resolvers/user.ex
@@ -4,4 +4,8 @@ defmodule FoodDiaryWeb.Resolvers.User do
   def get(%{id: user_id}, _context) do
     Users.Get.call(user_id)
   end
+
+  def create(%{input: parmas}, _context) do
+    Users.Create.call(parmas)
+  end
 end
