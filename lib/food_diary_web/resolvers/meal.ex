cddb74190721b376d67b39cc6dfa15abad7e93b5
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-09-07 10:07:53 -0300

    feat(MealType): publish meal to `new_meal`

--- lib/food_diary_web/resolvers/meal.ex
+++ lib/food_diary_web/resolvers/meal.ex
@@ -1,7 +1,14 @@
 defmodule FoodDiaryWeb.Resolvers.Meal do
+  alias Absinthe.Subscription
   alias FoodDiary.Meals
+  alias FoodDiaryWeb.Endpoint
 
   def create(%{input: parmas}, _context) do
-    Meals.Create.call(parmas)
+    with {:ok, meal} = response <- Meals.Create.call(parmas) do
+      Subscription.publish(Endpoint, meal, new_meal: "new_meal_topic")
+      response
+    else
+      error -> error
+    end
   end
 end
